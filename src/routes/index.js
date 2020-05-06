869995beaf6ff296f6f2c38639791db27dbb5c31
Alekzdevz <alekzdevz@gmail.com>
2020-05-05 23:22:17 -0500

    version nueva ipnradio stamp2

--- src/routes/index.js
+++ src/routes/index.js
@@ -61,7 +61,7 @@ router.post('/api/panel',  async(req, res) =>{
             callbackResponse.estadoEnergia = 'Inactivo';
     }
 
-    //callbackResponse.fecha = Date();
+    callbackResponse.fecha = new Date(callbackResponse.fecha);
     
     const callbackData =  new callbackDB(callbackResponse);
     await callbackData.save();
